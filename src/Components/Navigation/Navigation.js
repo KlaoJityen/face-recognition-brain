18ca9615718fe7c68ae4bc9f95e4a718d80b3396
Klao Alamus <pklaoalamus@gmail.com>
2022-01-30 01:17:10 +0700

    Change text colour

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -3,7 +3,7 @@ import React from "react";
 const Navigation = () => {
     return (
         <nav style={{display: "flex", justifyContent: "flex-end"}}>
-            <p className="f3 link dim black underline pa3 pointer">Sign In</p>
+            <p className="f3 link dim white underline pa3 pointer">Sign In</p>
         </nav>
     )
 }
