d3edf6634ad00228c53a15cc052db1df90ff06b0
Klao Alamus <pklaoalamus@gmail.com>
2022-01-30 00:00:18 +0700

    Update tsparticles

--- src/App.js
+++ src/App.js
@@ -4,11 +4,28 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
+import Particles from 'react-tsparticles';
+
+
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 30,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+         <Particles className='particles'
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
         <Rank />
