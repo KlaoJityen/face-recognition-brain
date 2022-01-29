9774de1cca93664da8cc27368a90bb229612f297
Klao Alamus <pklaoalamus@gmail.com>
2022-01-30 01:09:41 +0700

    synch particles

--- src/App.js
+++ src/App.js
@@ -4,28 +4,13 @@ import ImageLinkForm from './Components/ImageLinkForm/ImageLinkForm';
 import Navigation from './Components/Navigation/Navigation';
 import Logo from './Components/Logo/Logo';
 import Rank from './Components/Rank/Rank';
-import Particles from 'react-tsparticles';
-
-
-const particlesOptions = {
-  particles: {
-    number: {
-      value: 30,
-      density: {
-        enable: true,
-        value_area: 800
-      }
-    }
-  }
-}
+import Particle from './Components/Particle/Particle';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-         <Particles className='particles'
-          params={particlesOptions}
-        />
+        <Particle />
         <Navigation />
         <Logo />
         <Rank />
